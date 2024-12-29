2f7a04ec6c6f0a165f1b107206045a89a0fc44bf
mohamedeyaad <mooeyad@gmail.com>
2024-12-29 03:00:38 +0200

    Remove build and installation for ros2_robot_mover_python

--- build/ros2_robot_mover_python/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/eyad/rt_ws/src/ros2_robot_mover_python/install/ros2_robot_mover_python'
